2ef4332ec2c0ade6215e4d10671d9f6e207d3779
ari201604d7 <2016047@iiitdmj.ac.in>
2020-05-31 21:37:36 +0530

    get and diplay the fetched data on screen

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -4,18 +4,46 @@ import Post from '../../components/Post/Post';
 import FullPost from '../../components/FullPost/FullPost';
 import NewPost from '../../components/NewPost/NewPost';
 import './Blog.css';
+import axios from 'axios';
 
 class Blog extends Component {
+    state = {
+        posts: [],
+        selectedPostId: null
+    }
+    componentDidMount(){
+        axios.get('https://jsonplaceholder.typicode.com/posts')
+            .then(request =>{
+                const post = request.data.slice(0,4);
+                const updatedPost = post.map(post =>{
+                    return {
+                        ...post,
+                        author:'Arihant'
+                    }
+                })
+                this.setState({posts:updatedPost});
+            });
+    }
+
+    postSelectHandler = (id) =>{
+        this.setState({selectedPostId: id});
+    }
+
     render () {
+        let posts = this.state.posts.map(i =>{
+            return <Post
+                     key={i.id}
+                     title={i.title}
+                     author={i.author} 
+                     clicked={()=>this.postSelectHandler(i.id)}/>;
+        })
         return (
             <div>
                 <section className="Posts">
-                    <Post />
-                    <Post />
-                    <Post />
+                  {posts}
                 </section>
                 <section>
-                    <FullPost />
+                    <FullPost id={this.state.selectedPostId}/>
                 </section>
                 <section>
                     <NewPost />
